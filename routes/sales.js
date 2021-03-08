f371b188c63f80c13d7ec04b482fd105c0ae289b
Vbabison <officialbookingssf@gmail.comm>
2021-03-08 17:41:27 -0500

    margin add to features and messages

--- routes/sales.js
+++ routes/sales.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //SALES ROUTES
-router.get("/mysales", (req, res) => {
+router.get("/sales", (req, res) => {
   res.render("sales");
 });
 
