0b064cd58520169b882eafc26239cb1b08ed8566
OdeliaFink <odelia.finkel@gmail.com>
2021-03-09 00:15:35 +0000

    Added Jquery to messages and updated header

--- routes/messages.js
+++ routes/messages.js
@@ -3,20 +3,21 @@ const router = express.Router();
 
 module.exports = (db) => {
   //MESSAGE ROUTES
-  // need to add messages and conversation_id to DB
-  router.get("/conversations", (req, res) => {
+  // need to add messages and conversation_id to ERD
+  router.get("/messages", (req, res) => {
     const sql = `SELECT * FROM messages WHERE conversation_id = $1`;
     const conversationId = req.params.id;
+    //console.log
 
     db.query(sql, [conversationId]).then((data) => {
-      //insert template vars
+      //insert template vars for messages
       res.render("messages");
     });
   });
 
-  router.post("/messages", (req, res) => {
-    res.render("messages");
-    //AJAX, template for messages, passing in user input as var, and render to page
-  });
+  // router.post("/messages", (req, res) => {
+  //   res.render("messages");
+  //   //AJAX, template for messages, passing in user input as var, and render to page
+  // });
   return router;
 };
