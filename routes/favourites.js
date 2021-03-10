45f9a761ee457fca24ec01ca21fab43ff6cb773a
masinette <masinette.ts@gmail.com>
2021-03-10 13:33:37 -0500

    testing db connection

--- routes/favourites.js
+++ routes/favourites.js
@@ -1,13 +1,58 @@
 const express = require("express");
 const router = express.Router();
 
-module.exports = (db) => {
-  //FAVOURITE ROUTES
-  //favourites GET route
-  router.get("/favourites", (req, res) => {
-    res.render("favourites");
-  });
-  router.post("/favourites", (req, res) => {});
+// module.exports = (db) => {
+//   //FAVOURITE ROUTES
+//   //favourites GET route
+//   router.get("/favourites", (req, res) => {
+//     res.render("favourites");
+//   });
+//   router.post("/favourites", (req, res) => {});
 
+//   return router;
+// };
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    const sqlQuery = `SELECT * FROM favourites JOIN items ON item_id = items.id WHERE favourites.id IN (SELECT favourites.id FROM favourites);`;
+    db.query(sqlQuery)
+      .then(data => {
+        const templateVars = { items: data.rows };
+        res.render("favourites", templateVars);
+      })
+      .catch(err => {
+        res.status(!200).json({ error: err.message });
+      });
+  });
+
+  router.post("/", (req, res) => {
+    console.log(req.body);
+    const itemId = req.body.item_Id;
+    const userId = req.session.user_id;
+    console.log("ItemID: ", itemId);
+    console.log("UserId ", userId);
+    const sql = `INSER INTO favourites (userId, itemId) VALUES ($1, $2) RETURNING *;`
+    db.query(sql, [userId, itemId])
+    .then(data => {
+    res.redirect("/")
+     })
+    .catch(err => {
+    res.status(!200).jason({error: err.message});
+  });
+  });
+  /*
+    router.post("/delete", (req, res) => {
+      const itemId = req.body.itemId;
+      const userId = req.session.user_id;
+      const sql = `DELETE FROM favourites WHERE user_id = $1 AND item_id = $2 RETURNING *`
+      db.query(sql, [userId, itemId])
+      .then(data => {
+        res.redirect("/")
+      })
+      .catch(err => {
+        res.status(!200).json({error: errmessage});
+      })
+    })
+  */
   return router;
 };
--- routes/messages.js
+++ routes/messages.js
@@ -4,20 +4,65 @@ const router = express.Router();
 module.exports = (db) => {
   //MESSAGE ROUTES
   // need to add messages and conversation_id to ERD
-  router.get("/messages", (req, res) => {
-    const sql = `SELECT * FROM messages WHERE conversation_id = $1`;
+  router.get("/", (req, res) => {
+    console.log("in the messages");
+    const sql = `SELECT * FROM messages`;
     const conversationId = req.params.id;
-    //console.log
+    // console.log("DATABASE", db);
 
-    db.query(sql, [conversationId]).then((data) => {
-      //insert template vars for messages
-      res.render("messages");
-    });
+    return db.query(sql)
+      // .then((data) => {
+      //   //insert template vars for messages
+      // res.render("messages", );
+      // });
+      .then(data => {
+        console.log("in the messages");
+
+        return res.render("messages");
+
+
+      })
+      .catch(error => console.log(error));
+
   });
 
   // router.post("/messages", (req, res) => {
   //   res.render("messages");
   //   //AJAX, template for messages, passing in user input as var, and render to page
   // });
+  /*
+   module.exports = (db) => {
+     router.get("/messages/:id", (req,res) =>
+     const sql = "SELECT * FROM messages WHERE message_id = $1";
+     const messageId = req.params.id;
+     const params = [messageId]
+     db.query(sql, params)
+     .then(data => {
+       const templateVars = { messages: data.rows, messageId: messageId }
+      console.log("msgs", templateVars.messages)
+      res.render("messages", templateVars);
+      })
+     )};
+
+     router.post("/:id", (req, res) => {
+       const msg = req.body.message;
+       const sender = req.session.user_id;
+
+       const sql1 = "SELECT from_user FROM messages WHERE id = $1";
+       const conversaion = req.params.id;
+
+       db.query(sql1, [conversation]);
+       .then(data => {
+
+        const buyerId = data.rows[0].from_user;
+        let fromBuyer = false
+        console.log("sender", sender)
+        console.log("buyer", buyerId)
+
+        if
+
+       })
+      })
+  */
   return router;
 };
