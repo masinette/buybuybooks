4d700743e25ea8203e3f11d7fdd22e9a9c1bd10b
masinette <masinette.ts@gmail.com>
2021-03-11 00:15:05 -0500

    attach sales page to db

--- routes/sales.js
+++ routes/sales.js
@@ -9,12 +9,13 @@ module.exports = (db) => {
   //MESSAGE ROUTES
   // need to add messages and conversation_id to ERD
   router.get("/", (req, res) => {
-    const sql = `SELECT * FROM items`;
+    const sql = `SELECT * FROM items JOIN messages ON items.id = messages.item_id JOIN users ON buyer_id = users.id;`;
 
     return db.query(sql)
 
       .then(data => {
-        const templateVars = { messages:data.rows, current_user_id: req.session.user_id};
+        const templateVars = { items:data.rows, current_user_id: req.session.user_id};
+        // console.log("ITEMS", templateVars)
         return res.render("sales", templateVars);
       })
       .catch(error => console.log(error));
