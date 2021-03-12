f0357648c64673897728c29f9b4e82b8674f6a7f
masinette <masinette.ts@gmail.com>
2021-03-12 03:51:01 -0500

    PP with Bobby, save work before revert to old version

--- routes/messages.js
+++ routes/messages.js
@@ -6,43 +6,57 @@ module.exports = (db) => {
   //MESSAGE ROUTES
   // need to add messages and conversation_id to ERD
   router.get("/", (req, res) => {
-    const messagesQuery = `SELECT * FROM messages JOIN items ON items.id = item_id`;
+    const sqlQuery = `SELECT * FROM messages JOIN users ON users.id = sender_id WHERE messages.sender_id IN (SELECT messages.sender_id FROM messages WHERE sender_id = $1);`;
 
-    const itemsQuery = `SELECT id, name FROM items`;
-
-    return Promise.all([db.query(messagesQuery), db.query(itemsQuery)])
-
-      //data is an array of the return values from Promise.all
-      .then(data => {
-        // const templateVars = { messages: data.rows };
-
-        const messages = data[0].rows;
-        const items = data[1].rows;
-        const templateVars = { messages, items };
-        return res.render("messages", templateVars);
+    db.query(sqlQuery)
+      .then((data) => {
+        console.log("DATA", data.rows)
+        const templateVars = { messages: data.rows };
+        res.render("messages");
       })
-      .catch(error => console.log(error));
-  });
+      .catch((err) => {
+        res.status(!200).json({ error: err.message });
+      });
 
+    // const messagesQuery = `SELECT * FROM messages JOIN items ON items.id = item_id;`;
+    // const itemsQuery = `SELECT id, name FROM items;`;
+    // const usersQuery = `SELECT * FROM users;`;
+    // return Promise.all([db.query(messagesQuery), db.query(itemsQuery)])
+    //   //data is an array of the return values from Promise.all
+    //   .then(data => {
+    //     console.log("DATA RESULTS", data)
+    //     // const templateVars = { messages: data.rows };
+    //     const messages = data[0].rows;
+    //     console.log("messages RESULTS", messages)
+    //     const items = data[1].rows;
+    //     console.log("items RESULTS", items)
+    //     const templateVars = { messages, items };
+    //     console.log("items table", data[1])
+    //     return res.render("messages", templateVars);
+    //   })
+    //   .catch(error => console.log(error));
+  });
 
   router.post("/", (req, res) => {
     const message = req.body.message;
     const item_id = req.body.item;
-    const user_id = req.session.user_id;
-    const username = req.session.username;
     const convo_id = req.params.id;
 
-    console.log("USERNAME", username);
-    console.log(user_id);
-
-    if (!user_id) {
-      return res.redirect("/api/users/login");
-    }
     // query to get items table
     const sql = `SELECT * FROM items JOIN messages ON items.id = item_id;`;
     db.query(sql)
-
       .then(data => {
+
+
+        const name = req.body.name;
+        const price = req.body.item_price;
+        const image = req.body.image_url;
+        const description = req.body.item_description
+        const id = req.params.id;
+
+        const sql = `UPDATE items SET name = $3, image_url = $5, price = $2, description = $4 WHERE id = $1;`;
+    db.query(sql, [id, price, name, description, image])
+
         const sql = `INSERT INTO messages
       (message, sender_id, receiver_id, item_id , date_of_message)
       VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP);`;
@@ -55,9 +69,6 @@ module.exports = (db) => {
           })
           .catch(error => console.log(error));
       })
-
-    // const sql = `SELECT sender_id FROM messages WHERE id = $1;`
-
   });
   // router.get("/messages/:id", (req, res) => {
   //   const templateVars = { items: data.rows, current_user_id: req.session.user_id };
